f18057f85249b3e84b1af9c3091009c7939dc3e4
Zeeshan Ahmad Khalil <41861952+ZeeshanAhmadKhalil@users.noreply.github.com>
2023-10-10 19:16:14 +0500

    - showing issues on the front-end - added infinite scroll - created components and styling

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import {
+  Head,
+  Html,
+  Main,
+  NextScript
+} from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body
+      >
         <Main />
         <NextScript />
       </body>
